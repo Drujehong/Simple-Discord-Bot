64624b45270d67cf24d7ac17a1560d2d39ea1df5
Drujehong <drujehong@gmail.com>
2024-02-12 15:25:27 +0800

    Made a discord bot that just replies to the hello message from users
    More functionalities next up

---
+++ src/index.js
@@ -0,0 +1,34 @@
+// Install dotenv and load the environment variables
+require('dotenv').config();
+
+const { Client, IntentsBitField } = require('discord.js');
+
+// Access the token from the environment variables
+const token = process.env.TOKEN;
+
+// Client is the Bot
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds, // Intents are set of permissions, // Guilds are servers
+    IntentsBitField.Flags.GuildMembers, // GuildMembers are members inside a server
+    IntentsBitField.Flags.GuildMessages, // To listen to messages
+    IntentsBitField.Flags.MessageContent, // To read and access messages
+  ],
+});
+
+client.on('ready', (c) => {
+  console.log(`${c.user.tag} is ready to go!`);
+});
+
+client.on('messageCreate', (message) => {
+  if(message.author.bot) {
+    return;
+  }
+
+  if(message.content === 'hello'){
+    message.reply('hello');
+  }
+
+});
+
+client.login(process.env.TOKEN);
